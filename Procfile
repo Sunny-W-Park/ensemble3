42f58d7d467a4e0fce2fe8fc70b0f91d5e515e62
sunnyw <vnvnchlrh@gmail.com>
2020-08-12 15:29:53 +0900

    heroku edited

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ensemble2.wsgi --log-file -
--- ensemble_monday/settings.py
+++ ensemble_monday/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiceMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -161,3 +162,10 @@ LOGOUT_REDIRECT_URL = '/' #Post-Logout URL
 AUTH_USER_MODEL = "users.User" #Custom Auth Model
 
 #Hitcount
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
